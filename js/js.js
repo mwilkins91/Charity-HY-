f3cd5d5fc7d60c0b2aa573d3f625d0a62fe26312
Mark Wilkins <markwilkins3@hotmail.com>
2017-02-10 16:44:36 -0500

    Wokring on blog section, positioning done.

--- js/js.js
+++ js/js.js
@@ -2,4 +2,12 @@ $(function(){
 	$('.menuToggle').click(function(event) {
 		$('.navHamburger ul').toggleClass('hidden');
 	});
+
+	$('.main-carousel').flickity({
+		wrapAround: true,
+		autoPlay: 2000,
+		pageDots: false,
+		cellAlign: 'left',
+		contain: true
+	});
 })
