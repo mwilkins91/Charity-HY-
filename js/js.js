552a6ac4f55330db12881a5f293769b96fdb5a60
Mark Wilkins <markwilkins3@hotmail.com>
2017-02-14 21:03:00 -0500

    added svg animated load bars for causes section, added some hover states. Attempting circular load bar next.

--- js/js.js
+++ js/js.js
@@ -14,13 +14,13 @@ $(function(){
 	$('.accordianButton').click(function(event) {
 		if($(this).siblings('p').is(":visible")) {
 		$('.accordianButton').siblings('p').slideUp('slow');
-		$('.accordianButton').children('.accordianIcon').children('i').css('transform', 'initial');
+		$('.accordianButton').children('.accordianIcon').children('span').css('transform', 'initial');
 		}
 		else {
 			$('.accordianButton').siblings('p').slideUp('slow');
-			$('.accordianButton').children('.accordianIcon').children('i').css('transform', 'initial');
+			$('.accordianButton').children('.accordianIcon').children('span').css('transform', 'initial');
 			$(this).siblings('p').slideToggle('slow');
-			$(this).children('.accordianIcon').children('i').css('transform', 'rotate(90deg)');
+			$(this).children('.accordianIcon').children('span').css('transform', 'rotate(90deg)');
 
 		}
 	});
@@ -31,5 +31,39 @@ $(function(){
 			wrapAround: true
 		};
 		$('.carousel').flickity(options);
-		
+	
+
+	//from stackoverflow 
+	function isElementInViewport(elem) {
+	    var $elem = $(elem);
+
+	    // Get the scroll position of the page.
+	    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+	    var viewportTop = $(scrollElem).scrollTop();
+	    var viewportBottom = viewportTop + $(window).height();
+
+	    // Get the position of the element on the page.
+	    var elemTop = Math.round( $elem.offset().top );
+	    var elemBottom = elemTop + $elem.height();
+
+	    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+	}
+
+	// Check if it's time to start the animation.
+	function checkAnimation() {
+	    var $elem = $('.causeInfo .loadingBar svg');
+
+	    // If the animation has already been started
+	    if ($elem.hasClass('start')) return;
+
+	    if (isElementInViewport($elem)) {
+	        // Start the animation
+	        $elem.addClass('start');
+	    }
+	}
+
+	// Capture scroll events
+	$(window).scroll(function(){
+	    checkAnimation();
+	});	
 })
